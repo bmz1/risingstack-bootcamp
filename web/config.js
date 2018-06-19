f869f5c374b228928cc8defffc2cd7fe0c3b717b
Máté Boér <bmz@Mates-MacBook-Pro.local>
2018-06-19 13:48:03 +0200

    add step1

--- web/config.js
+++ web/config.js
@@ -1,7 +1,17 @@
 'use strict'
 
+const joi = require('joi')
+
+const schema = joi.object({
+  PORT: joi.number().min(0).max(65535)
+    .required()
+})
+  .unknown()
+
+const schemaVal = joi.attempt(process.env, schema)
+
 const config = {
-  port: 0
+  port: schemaVal.PORT
 }
 
 module.exports = config
